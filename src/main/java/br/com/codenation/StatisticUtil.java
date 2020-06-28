6c0e46dcd0031119cd0e4631ae3deef51350719c
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-28 18:37:30 -0300

    impementa os métodos do desafio

--- src/main/java/br/com/codenation/StatisticUtil.java
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,16 +1,49 @@
 package br.com.codenation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StatisticUtil {
 
 	public static int average(int[] elements) {
-		return 3;
+		int soma = 0;
+		for (int element : elements) {
+			soma += element;
+		}
+		return soma/elements.length;
 	}
 
 	public static int mode(int[] elements) {
-		return 3;
+		HashMap<Integer, Integer> repeatedNumbers = new HashMap<>();
+		int modeNumber = 0;
+		for (int i : elements) {
+			int count = 1;
+			for (int j : elements) {
+				if (i == j) {
+					repeatedNumbers.put(i, count++);
+				}
+				
+			}
+		}
+		for (Map.Entry<Integer, Integer> e: repeatedNumbers.entrySet()) {
+			Integer number = e.getKey();
+			Integer amountNumber = e.getValue();
+			if (amountNumber > modeNumber) {
+				modeNumber = number;
+			}
+		}
+		return modeNumber;
 	}
 
 	public static int median(int[] elements) {
-		return 3;
+		int resto = elements.length % 2;
+		int medianNumber;
+		if (resto == 0) {
+			medianNumber = elements.length / 2;
+			return (elements[medianNumber] + elements[medianNumber -1])/2;
+		} else {
+			medianNumber = elements.length / 2;
+			return elements[medianNumber];
+		}
 	}
 }
--- src/test/java/br/com/codenation/StatisticUtilTest.java
+++ src/test/java/br/com/codenation/StatisticUtilTest.java
@@ -9,15 +9,20 @@ public class StatisticUtilTest {
 	@Test
 	public void testAverage() {
 		assertEquals(3, StatisticUtil.average(new int[] {1,2,3,4,5}));
+		assertEquals(5, StatisticUtil.average(new int[] {2,9,4,5}));
 	}
 
 	@Test
 	public void testMode() {
 		assertEquals(3, StatisticUtil.mode(new int[] {1,2,3,3}));
+		assertEquals(2, StatisticUtil.mode(new int[] {2,2,1,2,3,3}));
+		assertEquals(4, StatisticUtil.mode(new int[] {1,2,2,3,3,4,4,4,5,5,6,7,8}));
 	}
 	
 	@Test
 	public void testMedian() {
 		assertEquals(3, StatisticUtil.median(new int[] {1,2,3,4,5}));
+		assertEquals(4, StatisticUtil.median(new int[] {1,2,3,4,5,6,7}));
+		assertEquals(8, StatisticUtil.median(new int[] {7,8,9}));
 	}
 }
