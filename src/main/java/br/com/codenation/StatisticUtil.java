69c6dc364faf8c252dfcecb00a34eafba9b595c0
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-28 14:03:41 -0300

    inicia controle de versão

---
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -0,0 +1,16 @@
+package br.com.codenation;
+
+public class StatisticUtil {
+
+	public static int average(int[] elements) {
+		return 3;
+	}
+
+	public static int mode(int[] elements) {
+		return 3;
+	}
+
+	public static int median(int[] elements) {
+		return 3;
+	}
+}
